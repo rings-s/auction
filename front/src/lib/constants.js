82ad27245a20b8ed1da1e51369fc735fc4ad2df7
rings-s <rings9619@gmail.com>
2025-06-12 20:14:17 -0400

    the latest errors

--- front/src/lib/constants.js
+++ front/src/lib/constants.js
@@ -1,6 +1,9 @@
+// front/src/lib/constants.js
 // Development URLs - localhost
-export const API_BASE_URL = 'http://localhost:8451/api';
-export const WS_BASE_URL = 'ws://localhost:8451/ws';
+export const VITE_API_URL = "http://localhost:8451/api";
+export const VITE_WS_URL = "ws://localhost:8451/ws";
+export const API_BASE_URL = VITE_API_URL;
+export const WS_BASE_URL = VITE_WS_URL;
 
 export const AUTH_ENDPOINTS = {
   LOGIN: `${API_BASE_URL}/accounts/login/`,
